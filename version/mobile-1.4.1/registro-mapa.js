22ab6949bbe2797c35afd3f879fbc8e69b437bdc
lauroagronomo1976-blip <lauroagronomo1976@gmail.com>
2026-02-07 11:22:50 -0300

    Update registro-mapa.js

--- version/mobile-1.4.1/registro-mapa.js
+++ version/mobile-1.4.1/registro-mapa.js
@@ -1,1 +1,29 @@
+console.log("🟢 REGISTRO – MAPA PURO ATIVO");
+
+const map = L.map("map").setView([-15.78, -47.93], 5);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19
+}).addTo(map);
+
+let pontoAtual = null;
+
+document.getElementById("btnMarcarPonto").addEventListener("click", () => {
+  map.locate({ enableHighAccuracy: true });
+});
+
+map.on("locationfound", (e) => {
+  if (pontoAtual) {
+    map.removeLayer(pontoAtual);
+  }
+
+  map.setView(e.latlng, 17);
+
+  pontoAtual = L.marker(e.latlng)
+    .addTo(map)
+    .bindPopup("📍 Ponto marcado")
+    .openPopup();
+
+  console.log("📍 Coordenadas:", e.latlng);
+});
 
