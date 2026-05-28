18daeb0769c8c16b4c83b682ab4768645b624f67
lauroagronomo1976-blip <lauroagronomo1976@gmail.com>
2026-05-28 16:30:19 -0300

    Update menu.js

--- version/mobile-1.5.2.2/menu.js
+++ version/mobile-1.5.2.2/menu.js
@@ -13,3 +13,45 @@ function iniciarMenu(){
   }
 
 }
+
+console.log("menu.js carregado")
+
+const btnMenu =
+  document.getElementById("btnMenu")
+
+const menuLateral =
+  document.getElementById("menuLateral")
+
+btnMenu.onclick = () => {
+
+  menuLateral.classList.toggle("aberto")
+
+}
+
+/* =========================
+FUNÇÕES MENU
+========================= */
+
+function abrirFeicoes(){
+
+  alert("Abrir Feições")
+
+}
+
+function abrirGrades(){
+
+  alert("Abrir Grades")
+
+}
+
+function abrirRastros(){
+
+  alert("Abrir Rastros")
+
+}
+
+function exportarTudo(){
+
+  alert("Exportar Tudo")
+
+}
