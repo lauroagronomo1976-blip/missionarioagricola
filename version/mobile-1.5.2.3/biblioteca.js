fae9d177147cb438a44003cb67c297eb101b3873
lauroagronomo1976-blip <lauroagronomo1976@gmail.com>
2026-06-24 16:10:23 -0300

    Update biblioteca.js

--- version/mobile-1.5.2.3/biblioteca.js
+++ version/mobile-1.5.2.3/biblioteca.js
@@ -1,1 +1,28 @@
+/*=================================================
+
+Missionário Agrícola
+
+Módulo:
+Biblioteca
+
+Arquivo:
+biblioteca.js
+
+Versão:
+1.0.0
+
+=================================================*/
+
 console.log("biblioteca.js carregado");
+
+/*=========================================
+BIBLIOTECA
+=========================================*/
+
+function fecharBiblioteca(){
+
+    document.getElementById(
+        "janelaBiblioteca"
+    ).style.display = "none";
+
+}
